5fbe9f05c425462a3ef759327c643f2aed3adff4
elfnamm <inmingoon4@gmail.com>
2024-11-07 08:25:24 +0900

    2024-11-07 1. EUC, 보고서화면 적용 2. 팝업레이아웃 수정중.

--- src/views/exec/execEuc/execEuc.js
+++ src/views/exec/execEuc/execEuc.js
@@ -1,102 +1,45 @@
-import apiCommon from "../../../js/api/apiCommon.js?";
+let apiCommon;
+let lktUtil;
+
+if (!window.apiCommonModule || !window.lktUtilModule) {
+  window.apiCommonModule = import(
+    `../../../js/api/apiCommon.js?t=${Date.now()}`
+  );
+  window.lktUtilModule = import(`../../../js/util/lktUtil.js?t=${Date.now()}`);
+}
+
+apiCommon = (await window.apiCommonModule).default;
+lktUtil = (await window.lktUtilModule).default;
+
+const idPrefix = "#exec-execEuc-execEuc ";
+
+let workOrderGrid;
+let workOrderGridDetail;
 
 function onCreate() {
-  loadDataProc(null);
-  //loadDetailData();
+  // 왼쪽 그리드 설정
 
-  function loadData() {
-    var obj = {
-      lktHeader: {
-        type: "REQUEST",
-        call: "PAGE.PATCH.CORES.USER",
-        status: 0,
-        message: "",
-        authentication:
-          "eyJjZW50ZXJDb2RlIjoiTEtUIiwiY2xpZW50Q29kZSI6IkxLVCIsIndhcmVob3VzZUNvZGUiOiJMS1QiLCJkYXRhYmFzZSI6eyJzZXJ2ZXIiOiIyMTEuMTEwLjIyOS4yMzkiLCJwb3J0IjoiMzMwNiIsImRhdGFiYXNlIjoiTEtUIiwidXNlcm5hbWUiOiJzcGMiLCJwYXNzd29yZCI6IjEwMTBxcHFwITNNIiwgImF0dHJpYnV0ZTAxIjoiTVlTUUwifSwid2FzIjp7InNlcnZlciI6IjIxMS4xMTAuMjI5LjIzOSIsInBvcnQiOiIxNDMzIn0sIm1xdHQiOnsic2VydmVyIjoiMjExLjExMC4yMjkuMjM5IiwicG9ydCI6IjE0MzMiLCJ1c2VybmFtZSI6ImxrdDBkYmEwMF9sa3QwMCIsInBhc3N3b3JkIjoiZGxkbmR5ZCEzTSJ9fQ==",
-        userName: "LKT",
-        centerCode: "LKT",
-        clientCode: "LKT",
-        warehouseCode: "LKT"
-      },
-      lktBody: [{}]
-    };
-
-    var encoded = btoa(JSON.stringify(obj));
-
-    apiCommon
-      .enduserComputing(encoded)
-      .done(function (response) {})
-      //loadData(response.lktBody);
-      .fail(function () {
-        // 에러 발생 시 처리
-        alert("error");
-      });
-  }
-
-  function loadDataDeatil() {
-    var obj = {
-      lktHeader: {
-        type: "REQUEST",
-        call: "PAGE.GET.CORES.ENDUSER.COMPUTING.EXECUTE",
-        status: 0,
-        message: "",
-        authentication:
-          "eyJjZW50ZXJDb2RlIjoiTEtUIiwiY2xpZW50Q29kZSI6IkxLVCIsIndhcmVob3VzZUNvZGUiOiJMS1QiLCJkYXRhYmFzZSI6eyJzZXJ2ZXIiOiIyMTEuMTEwLjIyOS4yMzkiLCJwb3J0IjoiMzMwNiIsImRhdGFiYXNlIjoiTEtUIiwidXNlcm5hbWUiOiJzcGMiLCJwYXNzd29yZCI6IjEwMTBxcHFwITNNIiwgImF0dHJpYnV0ZTAxIjoiTVlTUUwifSwid2FzIjp7InNlcnZlciI6IjIxMS4xMTAuMjI5LjIzOSIsInBvcnQiOiIxNDMzIn0sIm1xdHQiOnsic2VydmVyIjoiMjExLjExMC4yMjkuMjM5IiwicG9ydCI6IjE0MzMiLCJ1c2VybmFtZSI6ImxrdDBkYmEwMF9sa3QwMCIsInBhc3N3b3JkIjoiZGxkbmR5ZCEzTSJ9fQ==",
-        userName: "LKT",
-        centerCode: "LKT",
-        clientCode: "LKT",
-        warehouseCode: "LKT"
-      },
-      lktBody: [
-        {
-          centerCode: "HYN",
-          clientCode: "HY",
-          whrehouseCode: "HYN",
-          procedureCode: "SP_LKTPUB_SAMPLE"
-        }
-      ]
-    };
-    var encoded = btoa(JSON.stringify(obj));
-    apiCommon
-      .enduserComputingExecute(encoded)
-      .done(function (response) {})
-
-      .fail(function () {
-        alert("error");
-      });
-  }
-
-  function loadDataProc(aParam) {
-    if (aParam == null) {
-      aParam = [
-        {
-          macroCode: "SP_EUC_LIST",
-          macroName: "EUC 목록 조회",
-          parameters: [
-            {
-              parameterCode: "P_WORK_BATCH",
-              parameterName: "출고차수",
-              parameterPlaceholder: "출고차수",
-              parameterDesc: "출고차수"
-            },
-            {
-              parameterCode: "P_WORK_DATE",
-              parameterName: "출고일자",
-              parameterPlaceholder: "출고일자",
-              parameterDesc: "출고일자 (yyyymmdd)"
-            }
-          ]
-        }
-      ];
-    }
-
-    // 왼쪽 그리드 설정
-    $("#leftGrid").dxDataGrid({
-      dataSource: aParam, // 데이터 소스 설정
+  let headerCss = {
+    display: "flex",
+    "justify-content": "center",
+    "align-items": "center",
+    "text-align": "center"
+  };
+  workOrderGrid = $(idPrefix + "#workOrderGrid")
+    .dxDataGrid({
+      dataSource: [], // 데이터 소스 설정
       selection: {
         mode: "single" // 단일 셀렉션 모드
       },
-      columns: [{dataField: "macroName", caption: "EUC 목록 조회"}],
+      columns: [
+        {
+          dataField: "macroName",
+          caption: "EUC 목록",
+          headerCellTemplate: function (headerCell) {
+            headerCell.css(headerCss).text("EUC 목록");
+          }
+        }
+      ],
       showBorders: true,
       paging: {
         enabled: false // 페이지 기능 비활성화
@@ -106,120 +49,197 @@ function onCreate() {
         showDynamicPopup(selectedRowData);
         //alert(selectedRowData);
       }
-    });
-  }
+    })
+    .dxDataGrid("instance");
 
-  function loadDataDetailProc(aParam) {
-    if (aParam == null) {
-      aParam = [
-        {rst1: "test1", rst2: "테스트1"},
-        {rst1: "test2", rst2: "테스트2"}
-      ];
-
-      // 오른쪽 그리드 초기화
-      $("#rightGrid").dxDataGrid({
-        dataSource: aParam, // 데이터 소스 설정
-        columns: [
-          {dataField: "rst1", caption: "rst2"},
-          {dataField: "rst2", caption: "rst2"}
-        ],
-        selection: {
-          mode: "single" // 단일 셀렉션 모드
-        },
-        showBorders: true,
-        paging: {
-          pageSize: 10
-        },
-        pager: {
-          showPageSizeSelector: true,
-          allowedPageSizes: [10, 25, 50],
-          showInfo: true
-        }
-      });
-    }
-  }
-
-  // 동적 입력란 팝업 함수
-  function showDynamicPopup(rowData) {
-    // 입력란 배열을 동적으로 생성
-
-    let formItems = [];
-
-    for (const itm of rowData.parameters) {
-      let ftim = {
-        dataField: "parameterCode",
-        label: {text: itm.parameterName},
-        editorType: "dxTextBox"
-      };
-      formItems.push(ftim);
-    }
-
-    // 팝업 생성
-    $("#dynamicPopup")
-      .dxPopup({
-        title: rowData.macroName,
-        visible: true,
-        width: 400,
-        height: 300,
-        showCloseButton: true,
-        contentTemplate: function (contentElement) {
-          // 동적 폼 생성
-          $("<div>").appendTo(contentElement).dxForm({
-            formData: {},
-            items: formItems
-          });
-
-          // 실행, 취소 버튼 추가
-          $("<div>")
-            .appendTo(contentElement)
-            .dxButton({
-              text: "실행",
-              onClick: function () {
-                // euc 실행
-                //loadDataDeatil();
-                loadDataDetailProc(null);
-                $("#dynamicPopup").dxPopup("hide");
-              }
-            });
-
-          $("<div>")
-            .appendTo(contentElement)
-            .dxButton({
-              text: "취소",
-              onClick: function () {
-                $("#dynamicPopup").dxPopup("hide");
-              }
-            });
-        }
-      })
-      .dxPopup("show");
-  }
-
-  // 내보내기 버튼 클릭 이벤트
-  $("#exportButton").dxButton({
-    text: "내보내기",
-    onClick: function () {
-      const dataGrid = $("#rightGrid").dxDataGrid("instance");
-
-      // 엑셀 파일로 내보내기
-      DevExpress.excelExporter
-        .exportDataGrid({
-          component: dataGrid,
-          worksheet: new ExcelJS.Workbook().addWorksheet("Sheet 1")
-        })
-        .then(function (workbook) {
-          workbook.xlsx.writeBuffer().then(function (buffer) {
-            saveAs(
-              new Blob([buffer], {type: "application/octet-stream"}),
-              "rightGridData.xlsx"
-            );
-          });
-        });
-    }
-  });
+  searchList();
 }
 
 function onActive() {}
+
+// 동적 입력란 팝업 함수
+function showDynamicPopup(rowData) {
+  // 입력란 배열을 동적으로 생성
+
+  let formItems = [];
+
+  for (const itm of rowData.parameters) {
+    let ftim = {
+      dataField: "parameterCode",
+      label: {text: itm.parameterName},
+      editorType: "dxTextBox"
+    };
+    formItems.push(ftim);
+  }
+
+  // 팝업 생성
+  $(idPrefix + "#dynamicPopup")
+    .dxPopup({
+      title: rowData.macroName,
+      visible: true,
+      width: 400,
+      height: 300,
+      showCloseButton: true,
+      contentTemplate: function (contentElement) {
+        // 동적 폼 생성
+        $("<div>").appendTo(contentElement).dxForm({
+          formData: {},
+          items: formItems
+        });
+
+        // 실행, 취소 버튼 추가
+        $("<div>")
+          .appendTo(contentElement)
+          .dxButton({
+            text: "실행",
+            onClick: function () {
+              // euc 실행
+              //loadDataDeatil();
+              loadDataDetailProc(null);
+              $(idPrefix + "#dynamicPopup").dxPopup("hide");
+            }
+          });
+
+        $("<div>")
+          .appendTo(contentElement)
+          .dxButton({
+            text: "취소",
+            onClick: function () {
+              $(idPrefix + "#dynamicPopup").dxPopup("hide");
+            }
+          });
+      }
+    })
+    .dxPopup("show");
+}
+
+// 내보내기 버튼 클릭 이벤트
+$("#btnExport").dxButton({
+  stylingMode: "contained",
+  type: "default",
+  width: "100px",
+  text: "내보내기",
+  onClick: function () {
+    const dataGrid = $(idPrefix + "#workOrderGridDetail").dxDataGrid(
+      "instance"
+    );
+
+    // 엑셀 파일로 내보내기
+    DevExpress.excelExporter
+      .exportDataGrid({
+        component: dataGrid,
+        worksheet: new ExcelJS.Workbook().addWorksheet("Sheet 1")
+      })
+      .then(function (workbook) {
+        workbook.xlsx.writeBuffer().then(function (buffer) {
+          saveAs(
+            new Blob([buffer], {type: "application/octet-stream"}),
+            "rightGridData.xlsx"
+          );
+        });
+      });
+  }
+});
+
+function loadDataProc(aParam) {
+  if (aParam == null) {
+    aParam = [
+      {
+        macroCode: "SP_EUC_LIST",
+        macroName: "EUC 목록 조회",
+        parameters: [
+          {
+            parameterCode: "P_WORK_BATCH",
+            parameterName: "출고차수",
+            parameterPlaceholder: "출고차수",
+            parameterDesc: "출고차수"
+          },
+          {
+            parameterCode: "P_WORK_DATE",
+            parameterName: "출고일자",
+            parameterPlaceholder: "출고일자",
+            parameterDesc: "출고일자 (yyyymmdd)"
+          }
+        ]
+      }
+    ];
+  }
+
+  workOrderGrid.option("dataSource", aParam);
+}
+
+function loadDataDetailProc(aParam) {
+  if (aParam == null) {
+    aParam = [
+      {rst1: "test1", rst2: "테스트1"},
+      {rst1: "test2", rst2: "테스트2"}
+    ];
+
+    // 오른쪽 그리드 초기화
+    $(idPrefix + "#workOrderGridDetail").dxDataGrid({
+      dataSource: aParam, // 데이터 소스 설정
+      columns: [
+        {dataField: "rst1", caption: "rst2"},
+        {dataField: "rst2", caption: "rst2"}
+      ],
+      selection: {
+        mode: "single" // 단일 셀렉션 모드
+      },
+      showBorders: true,
+      paging: {
+        pageSize: 10
+      },
+      pager: {
+        showPageSizeSelector: true,
+        allowedPageSizes: [10, 25, 50],
+        showInfo: true
+      }
+    });
+  }
+}
+
+function searchList() {
+  loadDataProc(null);
+
+  var obj = {
+    lktHeader: lktUtil.getLktHeader("PAGE.GET.CORES.ENDUSER.COMPUTING"),
+    lktBody: [{}]
+  };
+
+  var encoded = btoa(JSON.stringify(obj));
+
+  apiCommon
+    .enduserComputing(encoded)
+    .done(function (response) {})
+    //loadData(response.lktBody);
+    .fail(function () {
+      // 에러 발생 시 처리
+      alert("error");
+    });
+}
+
+function searchListDetail(row) {
+  var obj = {
+    lktHeader: lktUtil.getLktHeader("PAGE.GET.CORES.ENDUSER.COMPUTING.EXECUTE"),
+    lktBody: [
+      {
+        centerCode: "HYN",
+        clientCode: "HY",
+        whrehouseCode: "HYN",
+        procedureCode: "SP_LKTPUB_SAMPLE"
+      }
+    ]
+  };
+
+  var encoded = btoa(JSON.stringify(obj));
+  apiCommon
+    .enduserComputingExecute(encoded)
+    .done(function (response) {})
+
+    .fail(function () {
+      alert("error");
+    });
+}
 
 export default {
   onCreate,
--- src/views/exec/execReport/execReport.js
+++ src/views/exec/execReport/execReport.js
@@ -1,103 +1,44 @@
-import apiCommon from "../../../js/api/apiCommon.js?";
+let apiCommon;
+let lktUtil;
 
-$(document).ready(function () {
-  //loadData();
-  loadDataProc(null);
-  //loadDetailData();
+if (!window.apiCommonModule || !window.lktUtilModule) {
+  window.apiCommonModule = import(
+    `../../../js/api/apiCommon.js?t=${Date.now()}`
+  );
+  window.lktUtilModule = import(`../../../js/util/lktUtil.js?t=${Date.now()}`);
+}
 
-  function loadData() {
-    var obj = {
-      lktHeader: {
-        type: "REQUEST",
-        call: "PAGE.GET.REPORT.VIEWER",
-        status: 0,
-        message: "",
-        authentication:
-          "eyJjZW50ZXJDb2RlIjoiTEtUIiwiY2xpZW50Q29kZSI6IkxLVCIsIndhcmVob3VzZUNvZGUiOiJMS1QiLCJkYXRhYmFzZSI6eyJzZXJ2ZXIiOiIyMTEuMTEwLjIyOS4yMzkiLCJwb3J0IjoiMzMwNiIsImRhdGFiYXNlIjoiTEtUIiwidXNlcm5hbWUiOiJzcGMiLCJwYXNzd29yZCI6IjEwMTBxcHFwITNNIiwgImF0dHJpYnV0ZTAxIjoiTVlTUUwifSwid2FzIjp7InNlcnZlciI6IjIxMS4xMTAuMjI5LjIzOSIsInBvcnQiOiIxNDMzIn0sIm1xdHQiOnsic2VydmVyIjoiMjExLjExMC4yMjkuMjM5IiwicG9ydCI6IjE0MzMiLCJ1c2VybmFtZSI6ImxrdDBkYmEwMF9sa3QwMCIsInBhc3N3b3JkIjoiZGxkbmR5ZCEzTSJ9fQ==",
-        userName: "LKT",
-        centerCode: "LKT",
-        clientCode: "LKT",
-        warehouseCode: "LKT"
-      },
-      lktBody: [{}]
-    };
+apiCommon = (await window.apiCommonModule).default;
+lktUtil = (await window.lktUtilModule).default;
 
-    var encoded = btoa(JSON.stringify(obj));
+const idPrefix = "#exec-execReport-execReport ";
 
-    apiCommon
-      .reportViewer(encoded)
-      .done(function (response) {})
-      //loadData(response.lktBody);
-      .fail(function () {
-        // 에러 발생 시 처리
-        alert("error");
-      });
-  }
+let workOrderGrid;
 
-  function loadDataDeatil() {
-    var obj = {
-      lktHeader: {
-        type: "REQUEST",
-        call: "PAGE.GET.CORES.REPORT.VIEWER.EXECUTE",
-        status: 0,
-        message: "",
-        authentication:
-          "eyJjZW50ZXJDb2RlIjoiTEtUIiwiY2xpZW50Q29kZSI6IkxLVCIsIndhcmVob3VzZUNvZGUiOiJMS1QiLCJkYXRhYmFzZSI6eyJzZXJ2ZXIiOiIyMTEuMTEwLjIyOS4yMzkiLCJwb3J0IjoiMzMwNiIsImRhdGFiYXNlIjoiTEtUIiwidXNlcm5hbWUiOiJzcGMiLCJwYXNzd29yZCI6IjEwMTBxcHFwITNNIiwgImF0dHJpYnV0ZTAxIjoiTVlTUUwifSwid2FzIjp7InNlcnZlciI6IjIxMS4xMTAuMjI5LjIzOSIsInBvcnQiOiIxNDMzIn0sIm1xdHQiOnsic2VydmVyIjoiMjExLjExMC4yMjkuMjM5IiwicG9ydCI6IjE0MzMiLCJ1c2VybmFtZSI6ImxrdDBkYmEwMF9sa3QwMCIsInBhc3N3b3JkIjoiZGxkbmR5ZCEzTSJ9fQ==",
-        userName: "LKT",
-        centerCode: "LKT",
-        clientCode: "LKT",
-        warehouseCode: "LKT"
-      },
-      lktBody: [
-        {
-          centerCode: "HYN",
-          clientCode: "HY",
-          whrehouseCode: "HYN",
-          procedureCode: "SP_LKTPUB_SAMPLE"
-        }
-      ]
-    };
-    var encoded = btoa(JSON.stringify(obj));
-    apiCommon
-      .reportViewerExecute(encoded)
-      .done(function (response) {})
+function onCreate() {
+  // 왼쪽 그리드 설정
 
-      .fail(function () {
-        alert("error");
-      });
-  }
-
-  function loadDataProc(aParam) {
-    if (aParam == null) {
-      aParam = [
-        {
-          macroCode: "SP_REPORT_LIST",
-          macroName: "보고서 목록 조회",
-          parameters: [
-            {
-              parameterCode: "P_WORK_BATCH",
-              parameterName: "출고차수",
-              parameterPlaceholder: "출고차수",
-              parameterDesc: "출고차수"
-            },
-            {
-              parameterCode: "P_WORK_DATE",
-              parameterName: "출고일자",
-              parameterPlaceholder: "출고일자",
-              parameterDesc: "출고일자 (yyyymmdd)"
-            }
-          ]
-        }
-      ];
-    }
-
-    // 왼쪽 그리드 설정
-    $("#leftGrid").dxDataGrid({
-      dataSource: aParam, // 데이터 소스 설정
+  let headerCss = {
+    display: "flex",
+    "justify-content": "center",
+    "align-items": "center",
+    "text-align": "center"
+  };
+  workOrderGrid = $(idPrefix + "#workOrderGrid")
+    .dxDataGrid({
+      dataSource: [], // 데이터 소스 설정
       selection: {
         mode: "single" // 단일 셀렉션 모드
       },
-      columns: [{dataField: "macroName", caption: "보고서 목록 조회"}],
+      columns: [
+        {
+          dataField: "macroName",
+          caption: "보고서 목록",
+          headerCellTemplate: function (headerCell) {
+            headerCell.css(headerCss).text("보고서 목록");
+          }
+        }
+      ],
       showBorders: true,
       paging: {
         enabled: false // 페이지 기능 비활성화
@@ -107,122 +48,136 @@ $(document).ready(function () {
         showDynamicPopup(selectedRowData);
         //alert(selectedRowData);
       }
-    });
-  }
+    })
+    .dxDataGrid("instance");
 
-  function loadDataDetailProc(aParam) {
-    if (aParam == null) {
-      aParam = [
-        {rst1: "test1", rst2: "테스트1"},
-        {rst1: "test2", rst2: "테스트2"}
-      ];
-
-      // 오른쪽 그리드 초기화
-      $("#rightGrid").dxDataGrid({
-        dataSource: aParam, // 데이터 소스 설정
-        columns: [
-          {dataField: "rst1", caption: "rst2"},
-          {dataField: "rst2", caption: "rst2"}
-        ],
-        showBorders: true,
-        paging: {
-          pageSize: 10
-        },
-        selection: {
-          mode: "single" // 단일 셀렉션 모드
-        },
-        pager: {
-          showPageSizeSelector: true,
-          allowedPageSizes: [10, 25, 50],
-          showInfo: true
-        }
-      });
-    }
-  }
-
-  // 동적 입력란 팝업 함수
-  function showDynamicPopup(rowData) {
-    // 입력란 배열을 동적으로 생성
-
-    let formItems = [];
-
-    for (const itm of rowData.parameters) {
-      let ftim = {
-        dataField: "parameterCode",
-        label: {text: itm.parameterName},
-        editorType: "dxTextBox"
-      };
-      formItems.push(ftim);
-    }
-
-    // 팝업 생성
-    $("#dynamicPopup")
-      .dxPopup({
-        title: rowData.macroName,
-        visible: true,
-        width: 400,
-        height: 300,
-        showCloseButton: true,
-        contentTemplate: function (contentElement) {
-          // 동적 폼 생성
-          $("<div>").appendTo(contentElement).dxForm({
-            formData: {},
-            items: formItems
-          });
-
-          // 실행, 취소 버튼 추가
-          $("<div>")
-            .appendTo(contentElement)
-            .dxButton({
-              text: "실행",
-              onClick: function () {
-                // 보고서 실행
-                //loadDataDeatil();
-                loadDataDetailProc(null);
-                $("#dynamicPopup").dxPopup("hide");
-              }
-            });
-
-          $("<div>")
-            .appendTo(contentElement)
-            .dxButton({
-              text: "취소",
-              onClick: function () {
-                $("#dynamicPopup").dxPopup("hide");
-              }
-            });
-        }
-      })
-      .dxPopup("show");
-  }
-
-  // 내보내기 버튼 클릭 이벤트
-  $("#exportButton").dxButton({
-    text: "내보내기",
-    onClick: function () {
-      const dataGrid = $("#rightGrid").dxDataGrid("instance");
-
-      // 엑셀 파일로 내보내기
-      DevExpress.excelExporter
-        .exportDataGrid({
-          component: dataGrid,
-          worksheet: new ExcelJS.Workbook().addWorksheet("Sheet 1")
-        })
-        .then(function (workbook) {
-          workbook.xlsx.writeBuffer().then(function (buffer) {
-            saveAs(
-              new Blob([buffer], {type: "application/octet-stream"}),
-              "rightGridData.xlsx"
-            );
-          });
-        });
-    }
-  });
-});
-
-function onCreate() {}
+  searchList();
+}
 
 function onActive() {}
+
+// 동적 입력란 팝업 함수
+function showDynamicPopup(rowData) {
+  // 입력란 배열을 동적으로 생성
+
+  let formItems = [];
+
+  for (const itm of rowData.parameters) {
+    let ftim = {
+      dataField: "parameterCode",
+      label: {text: itm.parameterName},
+      editorType: "dxTextBox"
+    };
+    formItems.push(ftim);
+  }
+
+  // 팝업 생성
+  $(idPrefix + "#dynamicPopup")
+    .dxPopup({
+      title: rowData.macroName,
+      visible: true,
+      width: 400,
+      height: 300,
+      showCloseButton: true,
+      contentTemplate: function (contentElement) {
+        // 동적 폼 생성
+        $("<div>").appendTo(contentElement).dxForm({
+          formData: {},
+          items: formItems
+        });
+
+        // 실행, 취소 버튼 추가
+        $("<div>")
+          .appendTo(contentElement)
+          .dxButton({
+            text: "실행",
+            onClick: function () {
+              $(idPrefix + "#dynamicPopup").dxPopup("hide");
+            }
+          });
+
+        $("<div>")
+          .appendTo(contentElement)
+          .dxButton({
+            text: "취소",
+            onClick: function () {
+              $(idPrefix + "#dynamicPopup").dxPopup("hide");
+            }
+          });
+      }
+    })
+    .dxPopup("show");
+}
+
+function loadDataProc(aParam) {
+  if (aParam == null) {
+    aParam = [
+      {
+        macroCode: "SP_EUC_LIST",
+        macroName: "보고서 목록 조회",
+        parameters: [
+          {
+            parameterCode: "P_WORK_BATCH",
+            parameterName: "출고차수",
+            parameterPlaceholder: "출고차수",
+            parameterDesc: "출고차수"
+          },
+          {
+            parameterCode: "P_WORK_DATE",
+            parameterName: "출고일자",
+            parameterPlaceholder: "출고일자",
+            parameterDesc: "출고일자 (yyyymmdd)"
+          }
+        ]
+      }
+    ];
+  }
+
+  workOrderGrid.option("dataSource", aParam);
+}
+
+function searchList() {
+  loadDataProc(null);
+
+  var obj = {
+    lktHeader: lktUtil.getLktHeader("PAGE.GET.CORES.REPORT.VIEWER"),
+    lktBody: [{}]
+  };
+
+  var encoded = btoa(JSON.stringify(obj));
+
+  apiCommon
+    .reportViewer(encoded)
+    .done(function (response) {})
+    //loadData(response.lktBody);
+    .fail(function () {
+      // 에러 발생 시 처리
+      alert("error");
+    });
+}
+
+function searchListDetail(row) {
+  var obj = {
+    lktHeader: lktUtil.getLktHeader("PAGE.GET.CORES.REPORT.VIEWER.EXECUTE"),
+    lktBody: [
+      {
+        centerCode: "HYN",
+        clientCode: "HY",
+        whrehouseCode: "HYN",
+        procedureCode: "SP_LKTPUB_SAMPLE"
+      }
+    ]
+  };
+
+  var encoded = btoa(JSON.stringify(obj));
+  apiCommon
+    .reportViewerExcute(encoded)
+    .done(function (response) {})
+
+    .fail(function () {
+      alert("error");
+    });
+}
 
 export default {
   onCreate,
