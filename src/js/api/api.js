2ae2993e4c4a398bf0b5d23ebd44059b0ab0c9fc
elfnamm <inmingoon4@gmail.com>
2024-12-06 10:12:55 +0900

    2024-12-06 1. 로그인및 인증관련 에러팝업제거.

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -7,48 +7,48 @@ const baseUrl = "http://192.168.26.24:4132"; // 실서버
 
 // const baseUrl = "http://192.168.26.120:1129";
 
-$.ajaxSetup({
-  beforeSend: function (jqXHR, settings) {},
-  complete: function (jqXHR, textStatus) {
-    // {"readyState":4,"responseText":"","status":204,"statusText":"No Content"}
+// $.ajaxSetup({
+//   beforeSend: function (jqXHR, settings) {},
+//   complete: function (jqXHR, textStatus) {
+//     // {"readyState":4,"responseText":"","status":204,"statusText":"No Content"}
 
-    //console.log(jqXHR);
-    try {
-      if (jqXHR.status != 200) {
-        let msgTmp =
-          "api: " +
-          jqXHR.apiUrl +
-          "\r\n" +
-          "status: " +
-          jqXHR.status +
-          "\r\n" +
-          "message: " +
-          jqXHR.statusText;
+//     //console.log(jqXHR);
+//     try {
+//       if (jqXHR.status != 200) {
+//         let msgTmp =
+//           "api: " +
+//           jqXHR.apiUrl +
+//           "\r\n" +
+//           "status: " +
+//           jqXHR.status +
+//           "\r\n" +
+//           "message: " +
+//           jqXHR.statusText;
 
-        alert(msgTmp);
-      } else {
-        if (
-          jqXHR.responseJSON &&
-          jqXHR.responseJSON.lktHeader.statusCode != "01"
-        ) {
-          let msgTmp =
-            "api: " +
-            jqXHR.apiUrl +
-            "\r\n" +
-            "statusCode: " +
-            jqXHR.responseJSON.lktHeader.statusCode +
-            "\r\n" +
-            "message: " +
-            jqXHR.responseJSON.lktHeader.message;
+//         alert(msgTmp);
+//       } else {
+//         if (
+//           jqXHR.responseJSON &&
+//           jqXHR.responseJSON.lktHeader.statusCode != "01"
+//         ) {
+//           let msgTmp =
+//             "api: " +
+//             jqXHR.apiUrl +
+//             "\r\n" +
+//             "statusCode: " +
+//             jqXHR.responseJSON.lktHeader.statusCode +
+//             "\r\n" +
+//             "message: " +
+//             jqXHR.responseJSON.lktHeader.message;
 
-          alert(msgTmp);
-        }
-      }
-    } catch (ex) {
-      //alert("unknown error");
-    }
-  }
-});
+//           alert(msgTmp);
+//         }
+//       }
+//     } catch (ex) {
+//       //alert("unknown error");
+//     }
+//   }
+// });
 
 // 로그인 API 호출 함수
 function server(param) {
