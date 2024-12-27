d154193ff85dcb371b2a71f57ff43446345d8768
Sanghyun Yu <inmingoon4@gmail.com>
2024-12-27 09:26:52 +0900

    2024-12-27 1. 라이센스 적용.

--- src/js/app.js
+++ src/js/app.js
@@ -151,6 +151,10 @@ function createMenu() {
 }
 
 $(document).ready(function () {
+  let devKey =
+    "ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogIjBiNGNhZDEzLWI3ZmEtNDljOC05MjI5LWUxYTFjNGQ1ZTViNSIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjQxCn0=.TKB/YEgo0tLVriWAArXeOhJVDffyIw4Uhv5MjX3z5LfyVCgVpPbSHmboGvsUWPG+4tgBCJBnmXjxXykz/BIVD5oOV6ktWAsvC73UPAo+y+3XI+5csEjwUTJvZH0ysRBhWkbSCg==";
+  DevExpress.config({licenseKey: devKey});
+
   gate();
 
   checkSession();
