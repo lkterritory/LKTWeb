2ce486ab0dc2e875c1ec294c1540046468dee1f9
juwon0481 <juwon0481@lkterritory.com>
2025-02-20 17:09:32 +0900

    2025-02-20 1. 박스매핑 레이아웃 변경 2. 스캔데이터 조건에 따라 필드에 데이터 입력

--- src/views/avg/sorterBoxMapping/sorterBoxMapping.js
+++ src/views/avg/sorterBoxMapping/sorterBoxMapping.js
@@ -32,74 +32,156 @@ lktUtil = (await window.lktUtilModule).default;
 lktMqtt = (await window.lktMqttModule).default;
 
 const idPrefix = "#avg-sorterBoxMapping-sorterBoxMapping ";
-
 let boxCode;
 let locCode;
 let codeMessage;
 let textBoxInstance 
 let locationCodeInstance 
 let boxCodeInstance
-function onCreate() {
-  
+let searchTimeout; 
+let locationPattern = /^(10[1-9]|11[0-6])0[0-3][0-9]|10[1-6]040|11[0-6]040$/;
 
+function onCreate() {
   $(idPrefix + ".title span").text("PDA01");
 
-
+  // 박스 코드 필드 설정
   boxCodeInstance = $(idPrefix + "#txtBoxBarcode")
-  .dxTextBox({
-    placeholder: "박스코드 스캔",
-    value: "",
-    width: "400px",
-    onInitialized: function(e) {
-      setTimeout(() => e.component.focus(), 100);
-    },
-    onValueChanged: function(e) {
-      let inputValue = e.value.toUpperCase().replace(/[^A-Z0-9]/g, ''); // 영문 대문자와 숫자만
-      e.component.option("value", inputValue); // 필터링된 값으로 업데이트
-      boxCode = inputValue; 
-    },
-    onEnterKey: function(e) {
-      if (/^H\d{4}$/.test(boxCode)) { // h0001 ~ h9999 형태의 값인지 확인
-        $(idPrefix + "#txtLocBarcode").dxTextBox("instance").focus();
-      } else {
-        locCode = boxCode
-       
-        $(idPrefix + "#txtLocBarcode").dxTextBox("instance").option("value", locCode);
-        $(idPrefix + "#txtBoxBarcode").dxTextBox("instance").option("value", "");
-        
-      }
-    }
-  })
-  .dxTextBox("instance");
-
-  locationCodeInstance = $(idPrefix + "#txtLocBarcode")
-  .dxTextBox({
-    placeholder: "로케이션 코드 스캔",
-    value: "",
-    width: "400px",
-    onValueChanged: function(e) {
-      locCode = e.value;
-    },
-    onEnterKey: function(e) {
-      // 로케이션 코드 엔터 입력 시 추가 동작 필요하면 여기에 작성
-      searchList();
-     
-    }
-  })
-  .dxTextBox("instance");
-
-   textBoxInstance = $(idPrefix + "#message")
     .dxTextBox({
-        value: "",
-        width: "300px",
-        readOnly: true // 읽기 전용 설정
+      placeholder: "박스코드 스캔",
+      value: "",
+      width: "100%",
+      onInitialized: (e) => setTimeout(() => e.component.focus(), 100),
+      onContentReady: (e) => e.component.element().find("input").attr("inputmode", "none"),
+      onValueChanged: (e) => handleBoxCodeChange(e),
+      onEnterKey: (e) => handleBoxCodeEnter(),
     })
     .dxTextBox("instance");
 
+  // 로케이션 코드 필드 설정
+  locationCodeInstance = $(idPrefix + "#txtLocBarcode")
+    .dxTextBox({
+      placeholder: "로케이션 코드 스캔",
+      value: "",
+      width: "100%",
+      onValueChanged: (e) => handleLocationCodeChange(e),
+      onContentReady: (e) => e.component.element().find("input").attr("inputmode", "none"),
+      onEnterKey: (e) => handleLocationCodeEnter(),
+    })
+    .dxTextBox("instance");
 
+  // 메시지 출력 텍스트박스 설정
+  textBoxInstance = $(idPrefix + "#message")
+    .dxTextBox({
+      value: "",
+      width: "100%",
+      readOnly: true,
+    })
+    .dxTextBox("instance");
+
+  // div 클릭 시 input으로 포커스 이동
+  makeDivFocusable(idPrefix + "#txtBoxBarcode");
+  makeDivFocusable(idPrefix + "#txtLocBarcode");
+
+  // 모든 다른 요소들에서 포커스 방지
+  preventFocusOnOtherElements()
 }
 
+// div 클릭 시 내부 input으로 포커스 이동
+function makeDivFocusable(divSelector) {
+  $(divSelector).on("click", function() {
+    const inputElement = $(this).find("input");
+    if (inputElement.length) {
+      inputElement.focus();  // div를 클릭하면 내부 input으로 포커스 이동
+    }
+  });
+}
 
+// 박스 코드 변경 시 처리
+function handleBoxCodeChange(e) {
+  let inputValue = e.value.toUpperCase().replace(/[^A-Z0-9]/g, ''); 
+  e.component.option("value", inputValue); 
+  boxCode = inputValue;
+}
+
+// 박스 코드 엔터키 입력 시 처리
+function handleBoxCodeEnter() {
+  if (/^H\d{4}$/.test(boxCode)) { // H0001 ~ H9999 패턴
+    locationCodeInstance.focus();
+  } else if(locationPattern.test(boxCode)) { // 정규식에 맞는 경우
+    locCode = boxCode;
+    locationCodeInstance.option("value", locCode);
+    boxCodeInstance.option("value", "");
+  } else { // 두 가지 조건에 모두 맞지 않으면 메시지 출력
+    textBoxInstance.option("value", "코드를 확인해주세요.");
+    boxCodeInstance.option("value", "");
+  }
+  checkAndSearch();
+}
+
+// 로케이션 코드 변경 시 처리
+function handleLocationCodeChange(e) {
+  locCode = e.value;
+}
+
+// 로케이션 코드 엔터키 입력 시 처리
+function handleLocationCodeEnter() {
+  if (locationPattern.test(locCode)) { // 정규식에 맞는 경우
+    locationCodeInstance.option("value", locCode);
+    boxCodeInstance.focus();
+  } else if (/^H\d{4}$/.test(locCode)) { // H0001 ~ H9999 패턴
+    boxCodeInstance.option("value", locCode); 
+    locationCodeInstance.option("value", "");
+  } else { // 두 가지 조건에 모두 맞지 않으면 메시지 출력
+    textBoxInstance.option("value", "코드를 확인해주세요");
+    locationCodeInstance.option("value", "");
+  }
+  checkAndSearch();
+}
+
+// 박스 코드와 로케이션 코드 값이 있을 때 searchList 실행
+function checkAndSearch() {
+  if (boxCode && locCode) {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(() => {
+      searchList();
+    }, 500);
+  }
+}
+
+// 박스 코드와 로케이션 코드 외의 다른 곳으로 포커스 이동 방지
+function preventFocusOnOtherElements() {
+  let isBoxFocused = false;
+  let isLocationFocused = false;
+
+  boxCodeInstance.on("focusin", function() {
+    isBoxFocused = true;
+  });
+
+  locationCodeInstance.on("focusin", function() {
+    isLocationFocused = true;
+  });
+
+  // 다른 곳을 클릭했을 때, 포커스가 이동하지 않도록 방지
+  $(document).on('mousedown', function(e) {
+    if (!(isBoxFocused || isLocationFocused)) {
+      e.preventDefault();
+    }
+  });
+
+  // 포커스가 바뀌면 포커스 상태 초기화
+  $(document).on('focusin', function(e) {
+    if ($(e.target).is(boxCodeInstance.element())) {
+      isBoxFocused = true;
+      isLocationFocused = false; // 로케이션 코드 포커스 해제
+    } else if ($(e.target).is(locationCodeInstance.element())) {
+      isLocationFocused = true;
+      isBoxFocused = false; // 박스 코드 포커스 해제
+    } else {
+      isBoxFocused = false;
+      isLocationFocused = false;
+    }
+  });
+}
 function onActive() {}
 
 
